b4c503f3e8561cb7af6c4ba1e836db582005db84
Komodo123 <ethananders97@gmail.com>
2023-04-10 23:13:26 -0700

    Added finalize and index.d.ts to play nice with typescript

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+export function chain(target = global, strict = false);
+
+class Chainable extends Function {
+  constructor(target?: Object | Function, strict?: boolean, finalize?: boolean);
+
+  then<X, Y>(
+    onfulfilled?: ((value: void) => X | PromiseLike<X> | null | undefined), 
+    onrejected?: ((reason: any) => Y | PromiseLike<Y> | null | undefined)
+  ): Promise;
+}
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-export function chain(target = global, strict = false) {
-  return new Chainable(target, strict);
+export function chain(target = global, strict = false, finalize = true) {
+  return new Chainable(target, strict, finalize);
 }
 
 export class Chainable extends Function {
-  constructor(target, strict) {
+  constructor(target, strict, finalize) {
     super();
 
     this.target = target || this;
@@ -14,6 +14,11 @@ export class Chainable extends Function {
     // circuit the chain and return false. Errors in the chain are still
     // catchable with Promise.catch.
     this.strict = strict || false;
+
+    // Automatically run "finally" if it is a function or return the property.
+    // This can be useful for builders when you don't want to manually call
+    // a build method at the end of a chain.
+    this.finalize = finalize || true;
 
     this.proxy = new Proxy(
       /* target */ this, 
@@ -28,10 +33,26 @@ export class Chainable extends Function {
   // Trap for [[Get]] used by operations such as property accessors.
   get(target, property) { 
     // console.log("[[Get]] " + property);
+    
+    if(property === "then" ||
+       property === "catch" ||
+       property === "finally") {
 
-    if (property === "then" ||
-        property === "catch" ||
-        property === "finally") {
+      if (this.finalize) {
+        // If the context has a finally method, run that before resolving the chain.
+        this.chain = this.chain.then((context) => {
+          if (this.defined(context, "finally")) {
+            if (typeof context.finally === "function") {
+              return context.finally();
+            } else {
+              return context.finally;
+            }
+          }
+
+          return context;
+        });
+      }
+
       let chain = this.chain[property].bind(this.chain);
 
       // We have to reset the chain to allow multiple await calls to be called
@@ -47,7 +68,7 @@ export class Chainable extends Function {
     // this.apply will be called immediately after and will use the [ context, 
     // callable ] tuple value this promise is returning.
     this.chain = this.chain.then((context) => {
-      if (!context || (context && typeof context[property] === "undefined")) {
+      if (!this.defined(context, property)) {
         if (this.strict) {
           throw new Error(`Property not defined: ${property} (the` +
             ` async-chain may have encountered an unexpected value)`);
@@ -75,7 +96,7 @@ export class Chainable extends Function {
   // Trap for [[Call]] used by operations such as function calls.
   apply(target, context, args) {
     // console.log("[[Call]]");
-
+    
     this.chain = this.chain.then((state) => {
       if (!Array.isArray(state)) {
         // A literal value was returned from [[Get]].
@@ -95,5 +116,9 @@ export class Chainable extends Function {
     // Seed the chain with the current object so the first [[Get]] trap starts
     // by calling methods on this object.
     this.chain = Promise.resolve(this.target);
+  }
+
+  defined(context, property) {
+    return context && typeof context[property] !== "undefined";
   }
 }
