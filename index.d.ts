76bad375a95bc42d0c6c815b2acea13cd01f7c22
Komodo123 <ethananders97@gmail.com>
2023-04-10 23:30:18 -0700

    Tightened types

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
-export function chain(target = global, strict = false);
+export function chain(target = global, strict = false): Chainable;
 
-class Chainable extends Function {
-  constructor(target?: Object | Function, strict?: boolean, finalize?: boolean);
+class Chainable<T> extends Function {
+  constructor(target?: Object | Function, strict?: boolean, finalize?: boolean): ProxyHandler;
 
   then<X, Y>(
-    onfulfilled?: ((value: void) => X | PromiseLike<X> | null | undefined), 
+    onfulfilled?: ((value: T) => X | PromiseLike<X> | null | undefined), 
     onrejected?: ((reason: any) => Y | PromiseLike<Y> | null | undefined)
-  ): Promise;
+  ): Promise<any>;
 
   catch<X>(
     onrejected?: ((reason: any) => X | PromiseLike<X> | null | undefined)
-  ): Promise;
+  ): Promise<any>;
 }
