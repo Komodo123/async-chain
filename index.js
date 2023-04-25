bbd17ca6ac66096c118d52c2f739c8116e497d46
Komodo123 <ethananders97@gmail.com>
2023-04-25 16:51:40 -0700

    Sync

--- index.js
+++ index.js
@@ -20,10 +20,7 @@ export class Chainable extends Function {
     // a build method at the end of a chain.
     this.finalize = finalize || true;
 
-    this.proxy = new Proxy(
-      /* target */ this, 
-      /* handler */ this
-    );
+    this.proxy = new Proxy(/* target */ this, /* handler */ this);
 
     this.reset();
 
@@ -31,13 +28,8 @@ export class Chainable extends Function {
   }
 
   // Trap for [[Get]] used by operations such as property accessors.
-  get(target, property) { 
-    // console.log("[[Get]] " + property);
-    
-    if(property === "then" ||
-       property === "catch" ||
-       property === "finally") {
-
+  get(target, property) {
+    if (property === "then" || property === "catch" || property === "finally") {
       if (this.finalize) {
         // If the context has a finally method, run that before resolving the chain.
         this.chain = this.chain.then((context) => {
@@ -60,27 +52,42 @@ export class Chainable extends Function {
       // a chain and .then, .catch, and .finally are always the final calls
       // in the chain.
       this.reset();
-      
+
       return chain;
     }
 
-    // Add the property or function to the chain. If it is a function, 
-    // this.apply will be called immediately after and will use the [ context, 
+    // Add the property or function to the chain. If it is a function,
+    // this.apply will be called immediately after and will use the [ context,
     // callable ] tuple value this promise is returning.
     this.chain = this.chain.then((context) => {
       if (!this.defined(context, property)) {
         if (this.strict) {
-          throw new Error(`Property not defined: ${property} (the` +
-            ` async-chain may have encountered an unexpected value)`);
+          throw new Error(
+            `Property not defined: ${property} (the` +
+              ` async-chain may have encountered an unexpected value)`
+          );
         } else {
+          if (context) {
+            if (context.constructor) {
+              console.warn(
+                `Property not defined: ${context.constructor.name}.${property}`
+              );
+            } else {
+              console.log(context.attr);
+              console.warn(
+                `Property not defined: ${context.name}[${property}]`
+              );
+            }
+          }
+
           return false;
         }
       }
-      
+
       switch (typeof context[property]) {
         case "function":
-          return [ context, context[property] ];
-          
+          return [context, context[property]];
+
         default:
         case "boolean":
         case "number":
@@ -96,20 +103,23 @@ export class Chainable extends Function {
   // Trap for [[Call]] used by operations such as function calls.
   apply(target, context, args) {
     // console.log("[[Call]]");
-    
+
     this.chain = this.chain.then(async (state) => {
       if (!Array.isArray(state)) {
         // A literal value was returned from [[Get]].
         return state;
       }
 
-      // The context and callee were returned because [[Get]] sent us a 
+      // The context and callee were returned because [[Get]] sent us a
       // callable.
       try {
-        let [ context, callee ] = state;
-        return await callee.apply(context, args);
+        let [context, callee] = state;
+        let result = await callee.apply(context, args);
+
+        return result;
       } catch (e) {
-        if(this.strict) {
+        // console.log("TEST");
+        if (this.strict) {
           throw e;
         }
 
